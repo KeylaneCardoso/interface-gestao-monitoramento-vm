a3a7093706498671726a7163b9f932d0f5f7a688
Keylane Cardoso <keylanecardoso28@gmail.com>
2024-06-28 13:57:48 -0300

    feat: criação da lógica para as ações da vm

--- src/app/pages/listing-vm/listing-vm.component.ts
+++ src/app/pages/listing-vm/listing-vm.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-listing-vm',
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrl: './listing-vm.component.css'
 })
 export class ListingVmComponent {
+
+  vm1Status = "RUNNING";
+  vm2Status = "PAUSED";
+  vm3Status = "STOP";
+
+  vms = [
+    { displayName: "vm1", cpu: 2, memory: 1024, status: "RUNNING"},
+    { displayName: "vm2", cpu: 1, memory: 512, status: "PAUSED"},
+    { displayName: "vm3", cpu: 4, memory: 2048, status: "STOP"},
+  ];
+
+  actionClick(vmIndex: number, actionName: string){
+    switch (actionName){
+
+      case "Iniciar":
+        if (this.vms[vmIndex].status === "RUNNING"){
+          alert("A VM já está em execução!")
+          return;
+        }
+        this.vms[vmIndex].status = 'RUNNING';
+        break;
+
+      case 'Pausar':
+        if (this.vms[vmIndex].status === 'PAUSED') {
+          alert('A VM já está parada!');
+          return;
+        }
+        else if(this.vms[vmIndex].status === 'STOP'){
+          alert('Não é possivel Pausar uma VM em STOP!');
+          return;
+        }
+        this.vms[vmIndex].status = 'PAUSED';
+        break;
+
+      case 'Parar':
+        if (this.vms[vmIndex].status === 'STOP') {
+          alert('A VM já está parada!');
+          return;
+        }
+        this.vms[vmIndex].status = 'STOP';
+        break;
+
+      case 'Excluir':
+        this.vms.splice(vmIndex, 1);
+        break;
+    }
+
+  }
 
 }
