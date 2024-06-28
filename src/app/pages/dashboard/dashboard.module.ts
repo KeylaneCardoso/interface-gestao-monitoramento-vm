b3fb285a079489bc0059fdd8d88e157b015593e8
Keylane Cardoso <keylanecardoso28@gmail.com>
2024-06-27 23:38:17 -0300

    feat: criação da page dashboard

---
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { DashboardComponent } from './dashboard.component';
+import { DashboardRoutingModule } from './dashboard-routing';
+
+@NgModule({
+  declarations: [
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    DashboardRoutingModule
+  ],
+  providers: [],
+  exports: [
+    DashboardComponent
+  ]
+
+})
+export class DashboardModule { }
