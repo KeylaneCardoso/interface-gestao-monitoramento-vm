9c1764a592596875915689e612dbe112c58bf110
Keylane Cardoso <keylanecardoso28@gmail.com>
2024-06-28 22:49:45 -0300

    feat: inserção dos gráficos

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+// import {Chart} from 'node_modules/chart.js'
+
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
   userName = "Keylane";
+
+  // @ViewChild("firstCanvas", {static: true}) elemento: ElementRef;
+
+  // constructor(private _elementRef: ElementRef) {
+  //   this.elemento = _elementRef.nativeElement;
+  // }
+
+  ngOnInit(){
+    // new Chart(this.elemento.nativeElement, {
+    //   type: 'line',
+    //   data: {
+    //     labels: ["teste1", "teste2", "teste3"],
+    //     datasets: [
+    //       {
+    //         data: [10, 20, 30]
+    //       }
+    //     ]
+    //   }
+    // });
+
+
+    this.RenderPieChart();
+    this.RenderBarChart()
+
+  }
+
+  RenderPieChart(){
+    const pieChart = new Chart( "piechart", {
+      type: 'pie',
+      data: {
+        labels: [
+          "VM's em uso",
+          "VM's disponiveis"
+        ],
+        datasets: [{
+          label: "Maquinas Virtuais",
+          data: [3, 10],
+          backgroundColor: [
+            '#00BBFF',
+            '#E8FFFE'
+          ],
+          hoverOffset: 4
+        }]
+      }
+    })
+
+  }
+
+  RenderBarChart(){
+    const barChart = new Chart( "barchart", {
+      type: 'bar',
+      data: {
+        labels: ["RUNNING", "PAUSED", "STOP"],
+        datasets: [{
+          label: "Status das VM's",
+          data: [1, 2, 3],
+          backgroundColor: [
+            '#FF8A08',
+            '#FFBE76',
+            '#FFB908'
+          ],
+          borderRadius: 5,
+        }]
+      },
+      options: {
+        indexAxis: 'y',
+        // gridLinesX: {
+        //   display: false
+        // }
+      },  
+    })
+
+  }
+
+
 }
